4bee84b99d4f923d2bacbe93323068878296b9b9
jsncorn <jsncourn@gmail.com>
2022-02-25 21:57:09 -0800

    added getallthoughts function

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -6,9 +6,27 @@ const thoughtController = {
 // /api/thoughts/
 // #############################
 
-//get all thoughts
+// -----------------------------
+// GET /thoughts
+// -----------------------------
+getAllThoughts(req, res) {
+    //will find all thoughts
+    Thought.find({})
+    //sort in descending order; oldest posts first
+    .sort({ _id: -1 })
+    .then(dbThoughtData => res.json(dbThoughtData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
+}
 
-//get a single thought by id
+// -----------------------------
+// GET /thoughts/:id
+// -----------------------------
+
+
+
 
 //post a new thought and push created thought id to
 //associated user's thougths array field
