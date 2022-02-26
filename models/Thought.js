03309501203e8cb4fadfd6e94f9f956dac1f2828
jsncorn <jsncourn@gmail.com>
2022-02-25 21:06:46 -0800

    added object skeleton

--- models/Thought.js
+++ models/Thought.js
@@ -1,3 +1,17 @@
 const { Schema, model } = require('mongoose');
 const Reaction = require('./Reaction');
 
+const thoughtSchema = new Schema({
+    thoughtText: {
+
+    },
+    createdAt: {
+
+    },
+    username: {
+
+    },
+    reactions: {
+        
+    }
+})
