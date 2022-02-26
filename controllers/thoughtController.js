f980c8448687f7a4b4be72fa583575529b2532d6
jsncorn <jsncourn@gmail.com>
2022-02-25 21:44:21 -0800

    added route outlines

---
+++ controllers/thoughtController.js
@@ -0,0 +1,30 @@
+const { User, Thought, Reaction } = require('../models');
+
+const thoughtController = {
+
+/*
+=============================
+/api/thoughts
+=============================
+*/
+
+//get all thoughts
+
+//get a single thought by id
+
+//post a new thought and push created thought id to
+//associated user's thougths array field
+
+/*
+=============================
+/ap/thoughts/:thoughtid/reactions
+=============================
+*/
+
+//post a reaction and push reaction to thought reaction array
+
+//delete a reaction and pull reaction from thought reaction array
+
+
+
+}
