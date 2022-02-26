07c15748bac1577ba201451bce677468c1672f05
jsncorn <jsncourn@gmail.com>
2022-02-25 23:22:40 -0800

    added routed links with associated commands

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -9,3 +9,8 @@ const {
     deleteReaction
 } = require('../../controllers/thoughtController');
 
+router.route('/').get(getAllThoughts).post(createThought);
+router.route('/:id').get(getThoughtById).put(updateThought).delete(deleteThought);
+router.route('/:thoughtId/reactions/').post(addReaction).delete(deleteReaction);
+
+module.exports = router;
