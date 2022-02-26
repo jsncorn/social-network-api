88e93581723b3a2536fab6ab238efb4af6d89ff9
jsncorn <jsncourn@gmail.com>
2022-02-25 21:10:05 -0800

    added object definitons

--- models/Thought.js
+++ models/Thought.js
@@ -1,17 +1,37 @@
 const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction');
 const Reaction = require('./Reaction');
 
 const thoughtSchema = new Schema({
     thoughtText: {
-
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 280
     },
     createdAt: {
-
+        type: Date,
+        defualt: Date.now,
+        get() {
+            return `${date.toLocaleString([], {
+                month: "long",
+                day: "numeric",
+                year: "numeric",
+                hour: "numeric",
+                minute: "numeric",
+                hour12: true,
+              })}`
+        }
     },
     username: {
-
+        type: String,
+        required: true
     },
-    reactions: {
-        
-    }
-})
+    reactions: [reactionSchema]
+}, {
+    toJSON: {
+        virtuals: true,
+        getters: true
+    },
+    id: false
+});
