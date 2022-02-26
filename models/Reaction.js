68763cdf2a6cc6206e735945abf47cc295e42e6f
jsncorn <jsncourn@gmail.com>
2022-02-25 21:04:41 -0800

    filled model objects with a date getter, formatted document

--- models/Reaction.js
+++ models/Reaction.js
@@ -1,18 +1,41 @@
-const { Schema, Types } = require('mongoose');
+const {
+    Schema,
+    Types
+} = require('mongoose');
 
 const reactionSchema = new Schema({
     reactionId: {
-
+        type: Types.ObjectId,
+        default: new Types.ObjectId()
     },
     reactionBody: {
-
+        type: String,
+        required: true,
+        maxLength: 280
     },
     username: {
-
+        type: String,
+        required: true
     },
     createdAt: {
-
+        type: Date,
+        default: Date.now,
+        get() {
+            return `${date.toLocaleString([], {
+                month: "long",
+                day: "numeric",
+                year: "numeric",
+                hour: "numeric",
+                minute: "numeric",
+                hour12: true,
+              })}`
+        }
     }
+}, {
+    toJSON: {
+        getters: true
+    },
+    id: false
 })
 
 module.exports = reactionSchema;
