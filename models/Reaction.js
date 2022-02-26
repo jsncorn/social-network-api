833770a4725eaa0bc182b8bd1aeb1e13ce5be3d8
jsncorn <jsncourn@gmail.com>
2022-02-25 20:59:32 -0800

    added reaction schema skeleton

---
+++ models/Reaction.js
@@ -0,0 +1,18 @@
+const { Schema, Types } = require('mongoose');
+
+const reactionSchema = new Schema({
+    reactionId: {
+
+    },
+    reactionBody: {
+
+    },
+    username: {
+
+    },
+    createdAt: {
+
+    }
+})
+
+module.exports = reactionSchema;
